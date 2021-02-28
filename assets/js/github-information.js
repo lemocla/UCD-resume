f610ee225a58da400b3dd89499e4f5b89e87d87d
lemocla <lemocla@icloud.com>
2021-02-28 21:57:17 +0000

    Add error message for error status 403 - too many requests

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -59,9 +59,13 @@ function fetchGitHubInformation(event) {
         $("#gh-user-data").html(userInformationHTML(userData));
         $("#gh-repo-data").html(repoInformationHTML(repoData));
     }, function(errorResponse){
-            if (errorResponse.Status === 404 ){
+            if (errorResponse.status === 404 ){
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
-            } else {
+            } else if(errorResponse.status === 403){
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            }
+            else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
